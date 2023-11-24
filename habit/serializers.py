d6d56eea8965162c30305aeed27ca2af7a20aae1
ShadeSWD <seawolfy60@gmail.com>
2023-11-25 00:33:33 +0300

    add validators

--- habit/serializers.py
+++ habit/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
 from habit.models import Habit
+from habit.validators import *
 
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
         fields = '__all__'
+
+    def validate(self, data):
+        validate_award(data)
+        validate_frequency(data)
+        validate_time_for_action(data)
+        return data
