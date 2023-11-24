e8f2bca0decc038a12b10422e5da2a5bf855c664
ShadeSWD <seawolfy60@gmail.com>
2023-11-25 00:36:47 +0300

    pagination

--- habit/views.py
+++ habit/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from habit.models import Habit
 from habit.serializers import HabitSerializer
+from habit.paginators import HabitPagination
 
 
 class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
+    pagination_class = HabitPagination
     queryset = Habit.objects.all()
